5540cb678f663ccbb0b8083a32d628fad2e96995
claudiu buma <claudiu.bruma@gmail.com>
2019-08-31 00:24:13 +0300

    added acccount controller and stub added customer controller and stub adde request and response models

---
+++ DutchPurpleFiat/DutchPurpleFiat/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchPurpleFiat.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchPurpleFiat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+
+        /// <summary>
+        /// open account
+        /// </summary>
+        /// <remarks>open account  - The API will expose an endpoint which accepts the user information (customerID, initialCredit)  for already existing customer</remarks>
+        /// <param name="body"></param>
+        /// <response code="200">account created succesfully</response>
+        /// <response code="400">well.... bad request is quite appropriately descriptive here</response>
+        /// <response code="404">Customer not found</response>
+        /// <response code="500">Something went wrong. Time to get to know your system admin and fellow developer</response>
+        [HttpPost]                
+        public async Task<IActionResult> AccountPost([FromBody]AccountModel body)
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, default(string));
+
+            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(400);
+
+            //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(404);
+
+            //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(500);
+
+            return new OkObjectResult(Guid.NewGuid());
+        }
+    }
+}
---
+++ DutchPurpleFiat/DutchPurpleFiat/Models/AccountModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchPurpleFiat.Models
+{
+    public class AccountModel
+    {
+        /// <summary>
+        /// Customer id identifying already existing user
+        /// </summary>
+        /// <value>Customer id identifying already existing user</value>
+        [Required]        
+        public string CustomerId { get; set; }
+
+        /// <summary>
+        /// Initial credit on opening new account
+        /// </summary>
+        /// <value>Initial credit on opening new account</value>
+        [Required]
+        public decimal? InitialCredit { get; set; }
+    }
+}
